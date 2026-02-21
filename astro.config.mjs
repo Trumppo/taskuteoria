d1cf99c2ed300034e69724b5054447d2a5d99fa2
trumppo <marko.mantere@gmail.com>
2026-02-21 11:01:39 +0000

    Fix base URL handling for GitHub Pages paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-const isProd = process.env.NODE_ENV === "production";
-
 export default defineConfig({
   site: "https://trumppo.github.io",
-  base: isProd ? "/taskuteoria" : "/",
+  base: "/taskuteoria/",
 });
